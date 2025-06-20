bb59004c5971efef687169e720c4563f33dadf48
jamesnulliu <jamesnulliu@gmail.com>
2025-06-20 06:36:12 +0000

    Remove LLVM install matrix - Now supports LLVM-21 by default

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,16 +4,20 @@ DOCKER_FILE=$1
 
 source ./scripts/.image-configs.sh
 
-if [[ "${DOCKER_FILE}" == *.cpu ]]; then IMAGE_TAG="${IMAGE_TAG}-cpu"; fi
-if [[ "${DOCKER_FILE}" == *.cuda ]]; then IMAGE_TAG="${IMAGE_TAG}-cuda${CUDA_VERSION}"; fi
-if [[ "${INSTALL_TORCH}" == "true" ]]; then IMAGE_TAG="${IMAGE_TAG}-torch${TORCH_VERSION}"; fi
-if [[ "${INSTALL_LLVM}" == "true" ]]; then IMAGE_TAG="${IMAGE_TAG}-llvm${LLVM_VERSION}"; fi
+if [[ "${DOCKER_FILE}" == *.cpu ]]; then 
+    IMAGE_TAG="${IMAGE_TAG}-cpu"
+elif [[ "${DOCKER_FILE}" == *.cuda ]]; then 
+    IMAGE_TAG="${IMAGE_TAG}-cuda${CUDA_VERSION}"
+fi
+
+if [[ "${INSTALL_TORCH}" == "true" ]]; then 
+    IMAGE_TAG="${IMAGE_TAG}-torch${TORCH_VERSION}"
+fi
 
 IMAGE_NAME=jamesnulliu/deeplearning:${IMAGE_TAG}
 
 docker build \
     -f $DOCKER_FILE \
-    --build-arg INSTALL_LLVM=$INSTALL_LLVM \
     --build-arg INSTALL_TORCH=$INSTALL_TORCH \
     --build-arg IMAGE_VERSION=$IMAGE_VERSION \
     --build-arg TORCH_VERSION=$TORCH_VERSION \
